525439d39be232a414efb8f5f1da8e8cd071d4ff
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2010-11-11 01:09:49 +0530

    Licence Info added

--- src/main/java/lk/appzone/client/MchoiceAventuraMessagingException.java
+++ src/main/java/lk/appzone/client/MchoiceAventuraMessagingException.java
@@ -1,23 +1,20 @@
-/*
- *   (C) Copyright 2010-2011 hSenid Software International (Pvt) Limited.
- *   All Rights Reserved.
- *
- *   These materials are unpublished, proprietary, confidential source code of
- *   hSenid Software International (Pvt) Limited and constitute a TRADE SECRET
- *   of hSenid Software International (Pvt) Limited.
- *
- *   hSenid Software International (Pvt) Limited retains all title to and intellectual
- *   property rights in these materials.
- *
+/**
+	Copyright [2010] [Arunoda Susiripala]
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+		http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
  */
 
 package lk.appzone.client;
 
-/**
- * $LastChangedDate: $
- * $LastChangedBy: $
- * $LastChangedRevision: $
- */
 
 public class MchoiceAventuraMessagingException extends Exception {
 
